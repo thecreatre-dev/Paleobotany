226b730764d3b6b2a11703d73611f8b5beac6f15
thecreatre-dev <dsylvestre603@gmail.com>
2026-03-22 13:58:41 -0400

    hallo

--- src/main/java/com/thecreatre/paleobotany/PaleobotanyDataGenerator.java
+++ src/main/java/com/thecreatre/paleobotany/PaleobotanyDataGenerator.java
@@ -2,7 +2,7 @@ package com.thecreatre.paleobotany;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-
+//hai//
 public class PaleobotanyDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
