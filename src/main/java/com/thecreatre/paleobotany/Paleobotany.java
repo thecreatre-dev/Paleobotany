4501a134939c77bfa8ef2b216cc26fcc09212f49
thecreatre-dev <dsylvestre603@gmail.com>
2026-03-22 13:41:15 -0400

    hai

---
+++ src/main/java/com/thecreatre/paleobotany/Paleobotany.java
@@ -0,0 +1,16 @@
+package com.thecreatre.paleobotany;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Paleobotany implements ModInitializer {
+	public static final String MOD_ID = "paleobotany";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/thecreatre/paleobotany/PaleobotanyClient.java
@@ -0,0 +1,10 @@
+package com.thecreatre.paleobotany;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PaleobotanyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
